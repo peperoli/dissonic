6f3094ed6ecad6e2b38a0d0fc9f227f71238953c
DoomyCrystal <shagix.ix@gmail.com>
2022-10-12 15:04:44 +0200

    Feat: Add real-time on concert edit, add delete dialog

--- components/EditConcertForm.js
+++ components/EditConcertForm.js
@@ -1,43 +1,40 @@
 import BandCheckbox from "./BandCheckbox"
 import { useState } from "react"
 import supabase from "../utils/supabase"
+import Button from "./Button"
 
-export default function EditConcertForm({ concert, bands, locations }) {
+export default function EditConcertForm({ concert, bands, locations, setIsOpen }) {
   let [selectedConcertBands, setSelectedConcertBands] = useState(concert.bands || [])
 
   async function handleSubmit(event) {
     event.preventDefault()
 
-    const { data: updateConcert, updateConcertError } = await supabase
-      .from('concerts')
-      .update({ 
-        date_start: event.target.dateStart.value, 
-        description: event.target.description.value,
-        bands: selectedConcertBands,
-        location: event.target.location.value,
-      })
-      .eq('id', concert.id)
+    try {
+      const { data, error } = await supabase
+        .from('concerts')
+        .update({ 
+          date_start: event.target.dateStart.value, 
+          description: event.target.description.value,
+          bands: selectedConcertBands,
+          location: event.target.location.value,
+        })
+        .eq('id', concert.id)
 
-    if (updateConcertError) {
-      console.error(updateConcertError)
+        if (error) {
+          throw error
+        }
+    } catch (error) {
+      alert(error.message)
     }
+
+    setIsOpen(false)
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <div className="form-control">
         <label htmlFor="dateStart">Datum</label>
         <input type="date" name="dateStart" id="dateStart" defaultValue={concert.date_start} />
       </div>
-      <p>Ausgewählte Bands:</p>
-      <ul className="flex gap-2">
-        {selectedConcertBands?.length > 0 ? selectedConcertBands.map(concertBand => (
-          <li key={concertBand} className="btn btn-tag">
-            {bands.find(band => band.id === concertBand)?.name}
-          </li>
-        )) : (
-          <p className="text-red">Dieses Konzert hat noch keine Bands.</p>
-        )}
-      </ul>
       <div className="form-control">
         <label htmlFor="location">Location</label>
         <select name="location" id="location" defaultValue={concert.location}>
@@ -47,23 +44,40 @@ export default function EditConcertForm({ concert, bands, locations }) {
           ))}
         </select>
       </div>
+      <div>
+        <p>Ausgewählte Bands:</p>
+        <ul className="flex gap-2">
+          {selectedConcertBands?.length > 0 ? selectedConcertBands.map(concertBand => (
+            <li key={concertBand} className="btn btn-tag">
+              {bands.find(band => band.id === concertBand)?.name}
+            </li>
+          )) : (
+            <p className="text-red">Dieses Konzert hat noch keine Bands.</p>
+          )}
+        </ul>
+      </div>
+      <fieldset className="form-control">
+        <legend>Bands</legend>
+        <div className="max-h-48 overflow-auto px-4 py-2 border border-slate-500 rounded-lg bg-slate-600">
+          {bands.map(band => (
+            <BandCheckbox
+              key={band.id}
+              concertId={concert.id}
+              band={band}
+              selectedConcertBands={selectedConcertBands}
+              setSelectedConcertBands={setSelectedConcertBands}
+            />
+          ))}
+        </div>
+      </fieldset>
       <div className="form-control">
         <label htmlFor="description">Beschreibung</label>
         <textarea name="description" id="description" defaultValue={concert.description} />
       </div>
-      <fieldset className="form-control">
-        <legend>Bands</legend>
-        {bands.map(band => (
-          <BandCheckbox
-            key={band.id}
-            concertId={concert.id}
-            band={band}
-            selectedConcertBands={selectedConcertBands}
-            setSelectedConcertBands={setSelectedConcertBands}
-          />
-        ))}
-      </fieldset>
-      <button type="submit" className="btn btn-primary">Speichern</button>
+      <div className="flex justify-end gap-4">
+        <Button onClick={() => setIsOpen(false)} label="Abbrechen" />
+        <button type="submit" className="btn btn-primary">Speichern</button>
+      </div>
     </form>
   )
 }
--- pages/concerts/[id].js
+++ pages/concerts/[id].js
@@ -1,8 +1,14 @@
 import supabase from "../../utils/supabase"
 import Link from "next/link"
 import EditConcertForm from "../../components/EditConcertForm"
-import { ArrowLeftIcon } from "@heroicons/react/24/solid"
+import { ArrowLeftIcon, CalendarIcon, MapPinIcon } from "@heroicons/react/24/solid"
 import { useEffect, useState } from "react"
+import PageWrapper from "../../components/PageWrapper"
+import Modal from "../../components/Modal"
+import Button from "../../components/Button"
+import dayjs from "dayjs"
+import 'dayjs/locale/de'
+import { useRouter } from "next/router"
 
 function BandSeenCheckbox({ band, bandsSeen, setBandsSeen }) {
   const isSeen = bandsSeen && bandsSeen.some(item => item === band.id) ? true : false
@@ -25,62 +31,133 @@ function BandSeenCheckbox({ band, bandsSeen, setBandsSeen }) {
         value={`seenBand${band.id}`}
         checked={isSeen}
         onChange={handleChange}
+        className="sr-only"
       />
       {band.name}
     </label>
   )
 }
 
-export default function ConcertPage({ concert, bands, locations, allBandsSeen }) {
+export default function ConcertPage({ initialConcert, bands, locations, allBandsSeen }) {
+  const [concert, setConcert] = useState(initialConcert)
   const [bandsSeen, setBandsSeen] = useState([])
+  const [editIsOpen, setEditIsOpen] = useState(false)
+  const [deleteIsOpen, setDeleteIsOpen] = useState(false)
+
+  const router = useRouter()
+  const dateFormat = new Date(concert.date_start).getFullYear() === new Date().getFullYear() ? 'DD. MMM' : 'DD. MMM YYYY'
+  const concertBands = bands.filter(band => concert.bands.some(bandId => band.id === bandId))
+  const location = locations.find(location => concert.location === location.id)
 
   useEffect(() => {
     const user = supabase.auth.user()
     setBandsSeen(allBandsSeen.find(bandsSeen => bandsSeen.user_id === user?.id)?.band_ids || [])
   }, [allBandsSeen])
-  
-  async function updateBandsSeen({}) {
-    const user = supabase.auth.user()
-    const { data: updatedBandsSeen, error} = await supabase
-    .from('bands_seen')
-    .upsert({
-      concert_id: concert.id,
-      user_id: user.id,
-      band_ids: bandsSeen,
-    },
-    {
-      onConflict: 'concert_id, user_id'
-    })
-  
-    if (error) {
-      console.error(error);
-    }
-  }
 
   useEffect(() => {
+    async function updateBandsSeen() {
+      const user = supabase.auth.user()
+      const { data: updatedBandsSeen, error } = await supabase
+        .from('bands_seen')
+        .upsert({
+          concert_id: concert.id,
+          user_id: user.id,
+          band_ids: bandsSeen,
+        },
+          {
+            onConflict: 'concert_id, user_id'
+          })
+
+      if (error) {
+        console.error(error);
+      }
+    }
+
     updateBandsSeen()
+  }, [bandsSeen, concert])
+
+  useEffect(() => {
+    const updateSubscription = supabase.from('concerts').on('UPDATE', payload => {
+      setConcert(payload.new)
+    }).subscribe()
+
+    return () => supabase.removeSubscription(updateSubscription)
   })
 
+  async function deleteConcert() {
+    const { error: deleteBandsSeenError } = await supabase.from('bands_seen').delete().eq('concert_id', concert.id)
+    const { error } = await supabase
+      .from('concerts')
+      .delete()
+      .eq('id', concert.id)
+
+    if (error || deleteBandsSeenError) {
+      console.error(error, deleteBandsSeenError)
+    } else {
+      router.push('/')
+    }
+  }
   return (
-    <main className="max-w-2xl p-8">
-      <Link href="/">
-        <a className="btn btn-link">
-          <ArrowLeftIcon className="h-text" />
-          Go Back
-        </a>
-      </Link>
-      <h1>Konzert<span className="ml-2 text-sm text-slate-500">{concert.id}</span></h1>
-      {concert.bands && concert.bands.map(bandId => (
-        <BandSeenCheckbox
-          key={bandId}
-          band={bands.find(item => item.id === bandId)}
-          bands={bands}
-          bandsSeen={bandsSeen}
-          setBandsSeen={setBandsSeen}
-        />
-      ))}
-      <EditConcertForm concert={concert} bands={bands} locations={locations} />
-    </main>
+    <PageWrapper>
+      <main className="max-w-2xl p-8">
+        <Link href="/">
+          <a className="btn btn-link">
+            <ArrowLeftIcon className="h-text" />
+            Go Back
+          </a>
+        </Link>
+        <h1>{concertBands[0]?.name} @ {location?.name}</h1>
+        {concertBands && concertBands.map(band => (
+          <BandSeenCheckbox
+            key={band.id}
+            band={band}
+            bands={concertBands}
+            bandsSeen={bandsSeen}
+            setBandsSeen={setBandsSeen}
+          />
+        ))}
+        <div className="flex gap-4 w-full">
+          <div className="inline-flex items-center">
+            <CalendarIcon className="h-text mr-2" />
+            {dayjs(concert.date_start).locale('de-ch').format(dateFormat)}
+          </div>
+          {location && (
+            <div className="inline-flex items-center">
+              <MapPinIcon className="h-text mr-2" />
+              {location.name}
+            </div>
+          )}
+        </div>
+        {concert.description && <p>{concert.description}</p>}
+        <div className="flex gap-4 mt-4">
+          <Button onClick={() => setEditIsOpen(true)} label="Bearbeiten" />
+          <Button onClick={() => setDeleteIsOpen(true)} label="Löschen" />
+        </div>
+        <Modal
+          isOpen={editIsOpen}
+          setIsOpen={setEditIsOpen}
+        >
+          <EditConcertForm
+            concert={concert}
+            bands={bands}
+            locations={locations}
+            setIsOpen={setEditIsOpen}
+          />
+        </Modal>
+        <Modal isOpen={deleteIsOpen} setIsOpen={setDeleteIsOpen}>
+        <div>
+          <h2>Konzert löschen</h2>
+          Willst du dieses Konzert wirklich löschen?
+          <div className="flex justify-end gap-3 mt-4">
+            <Button onClick={() => setDeleteIsOpen(false)} label="Abbrechen" />
+            <button onClick={deleteConcert} className="btn btn-primary btn-danger">
+              Löschen
+            </button>
+          </div>
+        </div>
+      </Modal>
+      </main>
+    </PageWrapper>
   )
 }
 
@@ -117,7 +194,7 @@ export async function getServerSideProps({ params }) {
 
   return {
     props: {
-      concert,
+      initialConcert: concert,
       bands,
       locations,
       allBandsSeen: bandsSeen,
