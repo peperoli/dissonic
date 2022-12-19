d2928b2d4da8f3574b52158dedd0911d7ef55943
DoomyCrystal <shagix.ix@gmail.com>
2022-12-19 17:55:42 +0100

    Bugfix: Define types for compare fn args, add missing viewport meta tag

--- app/head.js
+++ app/head.js
@@ -3,13 +3,8 @@ export default function Head() {
     <>
       <title>Concert Diary</title>
       <meta name="description" content="Keep track of your past concerts." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-        rel="stylesheet"
-      /> */}
     </>
   )
 }
--- components/profile/TopBands.tsx
+++ components/profile/TopBands.tsx
@@ -14,7 +14,7 @@ export const TopBands: FC<ITopBands> = ({ bands }) => {
 
   const highestCount = Math.max(...topBands.map(item => item.count))
 
-  function compare(a, b) {
+  function compare(a: { count: number }, b: { count: number }) {
     let comparison = 0
     if (a.count > b.count) {
       comparison = -1
@@ -31,17 +31,17 @@ export const TopBands: FC<ITopBands> = ({ bands }) => {
           .filter(item => item.count > 1)
           .sort(compare)
           .map(item => (
-            <div
-              key={item.id}
-              className="flex-shrink-0 w-22 overflow-hidden"
-            >
+            <div key={item.id} className="flex-shrink-0 w-22 overflow-hidden">
               <div className="relative flex items-end h-24 mb-2">
-                <div className="w-full rounded bg-venom" style={{ height: item.count / highestCount * 100 + '%' }} />
-                <p className="absolute w-full pb-1 text-center mix-blend-difference">{item.count}x</p>
+                <div
+                  className="w-full rounded bg-venom"
+                  style={{ height: (item.count / highestCount) * 100 + '%' }}
+                />
+                <p className="absolute w-full pb-1 text-center mix-blend-difference">
+                  {item.count}x
+                </p>
               </div>
-              <h3 className="text-center text-ellipsis overflow-hidden">
-                {item.name}
-              </h3>
+              <h3 className="text-center text-ellipsis overflow-hidden">{item.name}</h3>
             </div>
           ))}
       </div>
