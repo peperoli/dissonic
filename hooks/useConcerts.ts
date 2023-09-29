5af53c0d121c4619409b6446e950cee027f2324b
peperoli <shagix.ix@gmail.com>
2023-09-29 11:42:16 +0200

    Fix: Do not use `!inner` as it filters out items without join table rows

--- hooks/useConcerts.ts
+++ hooks/useConcerts.ts
@@ -6,28 +6,22 @@ import supabase from '../utils/supabase'
 const fetchConcerts = async (options?: FetchOptions): Promise<ExtendedRes<Concert[]>> => {
   let query = supabase.from('concerts').select(
     `id,
-      location:locations!inner(id),
-      bands!j_concert_bands!inner(id),
+      bands!j_concert_bands(id),
       bands_count:j_concert_bands(count),
-      bands_seen:j_bands_seen!inner(user_id)`,
+      bands_seen:j_bands_seen(user_id)`,
     { count: 'estimated' }
   )
 
-  if (options?.filter?.bands && options.filter.bands.length > 0) {
-    query = query.in('bands.id', options.filter.bands)
-  }
   if (options?.filter?.locations && options.filter.locations.length > 0) {
-    query = query.in('location.id', options.filter.locations)
+    query = query.in('location_id', options.filter.locations)
   }
+  
   if (options?.filter?.years && options.filter.years.length > 0) {
     query = query.gte('date_start', `${options.filter.years[0]}-01-01`)
     query = query.lte('date_start', `${options.filter.years[1]}-12-31`)
   }
-  if (options?.filter?.bandsSeenUser) {
-    query = query.eq('bands_seen.user_id', options.filter.bandsSeenUser)
-  }
 
-  const { data: ids, count: initialCount, error: countError } = await query
+  const { data: initalFilteredConcerts, count: initialCount, error: countError } = await query
 
   if (countError) {
     throw countError
@@ -35,18 +29,28 @@ const fetchConcerts = async (options?: FetchOptions): Promise<ExtendedRes<Concer
 
   const [from, to] = getPagination(options?.page ?? 0, options?.size ?? 24, initialCount ?? 0)
 
-  let filteredIds = ids.map(id => id.id) as string[]
+  let filteredConcerts = initalFilteredConcerts
+
+  if (options?.filter?.bands && options.filter.bands.length > 0) {
+    filteredConcerts = filteredConcerts?.filter(concert =>
+      concert.bands.some(band => options.filter?.bands?.includes(band.id))
+    )
+  }
+
+  if (options?.filter?.bandsSeenUser) {
+    filteredConcerts = filteredConcerts?.filter(concert =>
+      concert.bands_seen.some(band => options.filter?.bandsSeenUser === band.user_id)
+    )
+  }
 
   if (options?.filter?.bandsPerConcert) {
-    filteredIds = ids
-      ?.filter(
-        item =>
-          !options?.filter?.bandsPerConcert ||
-          (Array.isArray(item.bands_count) &&
-            item.bands_count[0].count >= options.filter.bandsPerConcert[0] &&
-            item.bands_count[0].count <= options?.filter?.bandsPerConcert[1])
-      )
-      .map(id => id.id)
+    filteredConcerts = filteredConcerts?.filter(
+      item =>
+        !options?.filter?.bandsPerConcert ||
+        (Array.isArray(item.bands_count) &&
+          item.bands_count[0].count >= options.filter.bandsPerConcert[0] &&
+          item.bands_count[0].count <= options?.filter?.bandsPerConcert[1])
+    )
   }
 
   let filteredQuery = supabase
@@ -58,7 +62,7 @@ const fetchConcerts = async (options?: FetchOptions): Promise<ExtendedRes<Concer
       bands_seen:j_bands_seen(band_id, user_id)`,
       { count: 'estimated' }
     )
-    .in('id', filteredIds)
+    .in('id', filteredConcerts.map(id => id.id) as string[])
 
   if (options?.page || options?.size) {
     filteredQuery = filteredQuery.range(from, to)
