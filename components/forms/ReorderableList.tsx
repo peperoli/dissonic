1a9851df1b9adbca5cb5246a0a52a703678a2821
peperoli <shagix.ix@gmail.com>
2023-12-11 16:07:54 +0100

    Enhancement: Enable horizontal reordering, use refs for dropzones

--- components/forms/ReorderableList.tsx
+++ components/forms/ReorderableList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { forwardRef, useEffect, useRef, useState } from 'react'
 import { Option } from '../../types/types'
 import clsx from 'clsx'
 
@@ -6,11 +6,15 @@ type DropZoneProps = {
   isVisible: boolean
 }
 
-function DropZone({ isVisible }: DropZoneProps) {
+const DropZone = forwardRef<HTMLDivElement, DropZoneProps>(({ isVisible }, ref) => {
   return (
-    <div className={clsx('drop-zone bg-slate-700', isVisible ? 'h-6' : 'h-0 overflow-hidden')} />
+    <div
+      ref={ref}
+      className={clsx('drop-zone bg-slate-700', isVisible ? 'w-4 h-6' : 'h-0 overflow-hidden')}
+    />
   )
-}
+})
+DropZone.displayName = 'DropZone'
 
 type ReorderableListItemProps = {
   item: string
@@ -18,11 +22,12 @@ type ReorderableListItemProps = {
   dragged: number | null
   setDragged: (value: number | null) => void
   dropZone: number
+  getRef: (node: HTMLDivElement | null, index: number) => void
 }
 
-function ReorderableListItem({ item, index, ...props }: ReorderableListItemProps) {
+function ReorderableListItem({ item, index, getRef, ...props }: ReorderableListItemProps) {
   return (
-    <div>
+    <div className="flex">
       <li
         role="button"
         onMouseDown={event => {
@@ -33,7 +38,10 @@ function ReorderableListItem({ item, index, ...props }: ReorderableListItemProps
       >
         {item}
       </li>
-      <DropZone isVisible={props.dragged !== null && props.dropZone === index + 1} />
+      <DropZone
+        ref={node => getRef(node, index + 1)}
+        isVisible={props.dragged !== null && props.dropZone === index + 1}
+      />
     </div>
   )
 }
@@ -59,6 +67,19 @@ export function ReorderableList({}: ReorderableListProps) {
   const [dragged, setDragged] = useState<number | null>(null)
   const [mouse, setMouse] = useState([0, 0])
   const [dropZone, setDropZone] = useState(0)
+  const dropZonesRef = useRef<Map<number, HTMLDivElement> | null>(null)
+
+  function getRef(node: HTMLDivElement | null, index: number) {
+    if (!dropZonesRef.current) {
+      dropZonesRef.current = new Map()
+    }
+    const map = dropZonesRef.current
+    if (node) {
+      map.set(index, node)
+    } else {
+      map.delete(index)
+    }
+  }
 
   useEffect(() => {
     function handleMouseMove(event: MouseEvent) {
@@ -70,12 +91,12 @@ export function ReorderableList({}: ReorderableListProps) {
   }, [])
 
   useEffect(() => {
-    if (dragged !== null) {
-      const dropZoneElements = Array.from(document.querySelectorAll('.drop-zone'))
-      console.log(dropZoneElements)
-
-      const elementPositions = dropZoneElements.map(element => element.getBoundingClientRect().top)
-      const absoluteDifferences = elementPositions.map(position => Math.abs(position - mouse[1]))
+    if (dragged !== null && dropZonesRef.current) {
+      const dropZoneElements = Array.from(dropZonesRef.current.values())
+      const elementPositions = dropZoneElements.map(element => element.getBoundingClientRect())
+      const absoluteDifferences = elementPositions.map(
+        position => Math.abs(position.left - mouse[0]) + Math.abs(position.top - mouse[1])
+      )
       let result = absoluteDifferences.indexOf(Math.min(...absoluteDifferences))
       if (result > dragged) {
         result += 1
@@ -132,37 +153,40 @@ export function ReorderableList({}: ReorderableListProps) {
     return () => document.removeEventListener('mouseup', handleMouseUp)
   })
   return (
-    <ul className="grid gap-1">
+    <>
       <button type="button" onClick={() => setItems(initialItems)}>
         Reset items
       </button>
-      {dragged !== null && (
-        <li
-          className="fixed z-50 -translate-y-1/2 bg-slate-750 shadow-xl"
-          style={{ top: `${mouse[1]}px` }}
-        >
-          {items[dragged]}
-        </li>
-      )}
-      <DropZone isVisible={dragged !== null && dropZone === 0} />
-      {items.map((item, index) => {
-        if (dragged !== index) {
-          return (
-            <ReorderableListItem
-              item={item}
-              index={index}
-              dragged={dragged}
-              setDragged={setDragged}
-              dropZone={dropZone}
-              key={index}
-            />
-          )
-        }
-      })}
+      <ul className="flex flex-wrap gap-1">
+        {dragged !== null && (
+          <li
+            className="fixed z-50 -translate-y-1/2 bg-slate-750 shadow-xl"
+            style={{ left: `${mouse[0]}px`, top: `${mouse[1]}px` }}
+          >
+            {items[dragged]}
+          </li>
+        )}
+        <DropZone ref={node => getRef(node, 0)} isVisible={dragged !== null && dropZone === 0} />
+        {items.map((item, index) => {
+          if (dragged !== index) {
+            return (
+              <ReorderableListItem
+                item={item}
+                index={index}
+                dragged={dragged}
+                setDragged={setDragged}
+                dropZone={dropZone}
+                getRef={getRef}
+                key={index}
+              />
+            )
+          }
+        })}
+      </ul>
       <pre>dragged: {JSON.stringify(dragged)}</pre>
       <pre>dropZone: {JSON.stringify(dropZone)}</pre>
       <pre>mouse: {JSON.stringify(mouse)}</pre>
       <pre className="text-sm">items: {JSON.stringify(items, null, 2)}</pre>
-    </ul>
+    </>
   )
 }
