a2fe021144d99f615304c463487c23898b46a207
peperoli <shagix.ix@gmail.com>
2024-11-16 20:50:51 +0100

    Detect language preference header

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,18 @@
 import { getRequestConfig } from 'next-intl/server'
-import { cookies } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 
 export default getRequestConfig(async () => {
+  const headersList = await headers()
   const cookieStore = await cookies()
-  const locale = cookieStore.get('locale')?.value || 'en-CH'
+  const languages = ['en', 'de']
+  const defaultLocale = 'de-CH'
+  const languageHeader = headersList.get('Accept-Language')
+  const matchingLanguage = languageHeader
+    ?.split(',')
+    .find(language => languages.includes(language.slice(0, 2)))
+  const localeCookie = cookieStore.get('locale')
+  const locale =
+    localeCookie?.value || (matchingLanguage && `${matchingLanguage}-CH`) || defaultLocale
 
   return {
     locale,
