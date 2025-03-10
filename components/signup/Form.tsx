36c541fe86c5c821e00c113ba4fc6bfda81cd677
peperoli <shagix.ix@gmail.com>
2025-03-10 22:35:42 +0100

    Refactor signup page components, add oath buttons

--- components/signup/Form.tsx
+++ components/signup/Form.tsx
@@ -1,18 +1,22 @@
-import { MutationStatus } from '@tanstack/react-query'
+'use client'
+
 import { useForm } from 'react-hook-form'
 import { useProfiles } from '../../hooks/profiles/useProfiles'
 import { usernameRegex } from '../../lib/usernameRegex'
 import { Button } from '../Button'
 import { TextField } from '../forms/TextField'
-import { SignUpFormData } from '../../actions/auth'
+import { signUp, SignUpFormData } from '../../actions/auth'
 import { useTranslations } from 'next-intl'
+import { StatusBanner } from '../forms/StatusBanner'
+import { useMutation } from '@tanstack/react-query'
+import { SiGoogle, SiSpotify } from '@icons-pack/react-simple-icons'
 
-type FormProps = {
-  onSubmit: (data: SignUpFormData) => void
-  status: MutationStatus
-}
+export const Form = () => {
+  const { mutate, status, error } = useMutation({
+    mutationFn: signUp,
+    onError: error => console.error(error),
+  })
 
-export const Form = ({ onSubmit, status }: FormProps) => {
   const {
     register,
     handleSubmit,
@@ -21,44 +25,73 @@ export const Form = ({ onSubmit, status }: FormProps) => {
   const { data: profiles } = useProfiles()
   const t = useTranslations('SignupForm')
   const usernames = profiles?.map(item => item.username)
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="grid gap-5">
-      <TextField
-        {...register('email', { required: true })}
-        error={errors.email}
-        type="email"
-        label={t('email')}
-        placeholder="william@delos.com"
-      />
-      <TextField
-        {...register('username', {
-          required: true,
-          minLength: { value: 4, message: t('usernameLengthError') },
-          maxLength: { value: 16, message: t('usernameLengthError') },
-          pattern: { value: usernameRegex, message: t('usernamePatternError') },
-          validate: value => !usernames?.includes(value ?? '') || t('usernameTakenError'),
-        })}
-        error={errors.username}
-        label={t('username')}
-      />
-      <TextField
-        {...register('password', {
-          required: true,
-          minLength: { value: 12, message: t('passwordLengthError') },
-        })}
-        error={errors.password}
-        type="password"
-        label={t('password')}
-        autoComplete="off"
-      />
-      <div>
+
+  return status === 'success' ? (
+    <StatusBanner statusType="success" message={t('successMessage')} className="mt-6" />
+  ) : (
+    <section className="rounded-lg bg-slate-800 p-6">
+      <form onSubmit={handleSubmit(formData => mutate(formData))} className="grid gap-5">
+        <TextField
+          {...register('email', { required: true })}
+          error={errors.email}
+          type="email"
+          label={t('email')}
+          placeholder="william@delos.com"
+        />
+        <TextField
+          {...register('username', {
+            required: true,
+            minLength: { value: 4, message: t('usernameLengthError') },
+            maxLength: { value: 16, message: t('usernameLengthError') },
+            pattern: { value: usernameRegex, message: t('usernamePatternError') },
+            validate: value => !usernames?.includes(value ?? '') || t('usernameTakenError'),
+          })}
+          error={errors.username}
+          label={t('username')}
+        />
+        <TextField
+          {...register('password', {
+            required: true,
+            minLength: { value: 12, message: t('passwordLengthError') },
+          })}
+          error={errors.password}
+          type="password"
+          label={t('password')}
+          autoComplete="off"
+        />
+        <div>
+          <Button
+            type="submit"
+            label={t('createAccount')}
+            appearance="primary"
+            loading={status === 'pending'}
+          />
+        </div>
+      </form>
+      {status === 'error' && (
+        <StatusBanner
+          statusType="error"
+          message={
+            'code' in error && error.code === '23505'
+              ? t('emailAlreadyInUseError')
+              : t(error.message)
+          }
+          className="mt-6"
+        />
+      )}
+      <div className="mt-6 grid gap-3">
+        <span className="section-headline">{t('or')}</span>
         <Button
-          type="submit"
-          label={t('createAccount')}
-          appearance="primary"
-          loading={status === 'pending'}
+          label={t('continueWithGoogle')}
+          icon={<SiGoogle className="size-icon" />}
+          appearance="secondary"
+        />
+        <Button
+          label={t('continueWithSpotify')}
+          icon={<SiSpotify className="size-icon" />}
+          appearance="secondary"
         />
       </div>
-    </form>
+    </section>
   )
 }
--- components/signup/SignupPage.tsx
+++ components/signup/SignupPage.tsx
@@ -1,25 +1,11 @@
-'use client'
-import { SubmitHandler } from 'react-hook-form'
-import { Button } from '../Button'
 import { CheckIcon } from 'lucide-react'
-import { useSignUp } from '../../hooks/auth/useSignup'
-import { StatusBanner } from '../forms/StatusBanner'
-import { useRouter } from 'next/navigation'
 import { Form } from './Form'
-import { AuthError, PostgrestError } from '@supabase/supabase-js'
-import { SignUpFormData } from '../../actions/auth'
-import { useTranslations } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
+import Link from 'next/link'
 
-export const SignupPage = () => {
-  const signUp = useSignUp()
-  const { push } = useRouter()
-  const t = useTranslations('SignupPage')
+export async function SignupPage() {
+  const t = await getTranslations('SignupPage')
 
-  const onSubmit: SubmitHandler<SignUpFormData> = async formData => {
-    signUp.mutate(formData)
-  }
-
-  const error = signUp.error as AuthError | PostgrestError
   return (
     <main className="container-sm">
       <section className="rounded-2xl bg-radial-gradient from-venom/50 p-8">
@@ -48,27 +34,11 @@ export const SignupPage = () => {
           </li>
         </ul>
       </section>
-      {signUp.status !== 'success' && (
-        <section className="rounded-lg bg-slate-800 p-6">
-          <Form onSubmit={onSubmit} status={signUp.status} />
-        </section>
-      )}
-      {signUp.status === 'error' && (
-        <StatusBanner
-          statusType="error"
-          message={
-            'code' in error && error.code === '23505'
-              ? t('emailAlreadyInUseError')
-              : t(error.message)
-          }
-          className="mt-6"
-        />
-      )}
-      {signUp.status === 'success' && (
-        <StatusBanner statusType="success" message={t('successMessage')} className="mt-6" />
-      )}
+      <Form />
       <h2 className="mt-10">{t('doYouHavAnAccountAlready')}</h2>
-      <Button label={t('login')} onClick={() => push('/login')} appearance="secondary" />
+      <Link href="/login" className="btn btn-secondary">
+        {t('login')}
+      </Link>
     </main>
   )
 }
